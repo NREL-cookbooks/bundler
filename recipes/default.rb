68ef05b4ee627304c4eb7c92b2f045b4599ae745
Muerdter, Nick <nick.muerdter@nrel.gov>
2010-11-30 21:28:46 +0000

    Add a cookbook for installing the ruby-oci8 gem.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,10 @@
+include_recipe "ruby_enterprise"
+
 execute "uninstall old bundlers" do
   command "gem uninstall -a bundler"
   only_if "gem list bundler | grep ,"
 end
 
-gem_package "bundler" do
+ree_gem "bundler" do
   version node[:bundler][:version]
 end
