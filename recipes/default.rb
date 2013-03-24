e9a431ebed41cb4d78af888f5013cf213436e93b
Nick Muerdter <nick.muerdter@nrel.gov>
2013-03-24 17:20:04 -0600

    Make sure bundler is installed in all rbenv rubies.

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@ include_recipe "rbenv::system"
 rbenv_gem "bundler" do
   version node[:bundler][:version]
 end
+
+node[:rbenv][:rubies].each do |version|
+  if version.is_a?(Hash)
+    version = version['name']
+  end
+
+  rbenv_gem "bundler" do
+    version node[:bundler][:version]
+    rbenv_version version
+  end
+end
