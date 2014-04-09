8e3feb73697fcb4a0c42fd4f1631ee3e5dc4a461
Nick Muerdter <nick.muerdter@nrel.gov>
2014-04-09 16:31:11 -0600

    Bump version.

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "bundler"
 maintainer        "37signals"
 maintainer_email  "sysadmins@37signals.com"
 description       "Configures gem bundler"
-version           "0.1.4"
+version           "0.1.5"
 
 depends "rbenv"
